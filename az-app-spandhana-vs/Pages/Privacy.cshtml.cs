a2b3a186b4f02225362102b442ec1a98c1bc2f14
spandhana.dotnetdev <spandhana.dotnetdev@gmail.com>
2024-02-20 13:24:11 -0600

    Added Key in Application.settings file

--- az-app-spandhana-vs/Pages/Privacy.cshtml.cs
+++ az-app-spandhana-vs/Pages/Privacy.cshtml.cs
@@ -6,15 +6,19 @@ namespace az_app_spandhana_vs.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger,IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
 
         public void OnGet()
         {
-            throw new Exception("testing Application Insight");
+           // throw new Exception("testing Application Insight");
+
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
